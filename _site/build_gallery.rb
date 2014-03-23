3a065ef91563cd14414ef1559f730ea6cd2aaa71
Corey Martella <cmartella@zynga.com>
2014-03-22 20:06:08 -0400

    full gallery

--- _site/build_gallery.rb
+++ _site/build_gallery.rb
@@ -5,24 +5,17 @@ end
 #resize all originals to regular image
 FileUtils.mkdir_p "img/gallery/large/"
 FileUtils.mkdir_p "img/gallery/thumb/"
-File.open("_portfolio.html", "w") do |html|
-  Dir["img/gallery/original/*.*"].sort_by{rand}.each do |f|
+FileUtils.mkdir_p "_includes"
+  widths = [1,1,1,1,1,1,1,1,2,2]
+File.open("_includes/gallery.html", "w") do |html|
+  Dir["img/gallery/original/*.*"].sort_by{rand}.each_with_index do |f,i|
     `convert #{f} -resize 1024x1024 img/gallery/large/#{f.split("/").last}` if !File.exists?("img/gallery/large/#{f.split("/").last}")
-    `convert #{f} -resize 300x300 img/gallery/thumb/#{f.split("/").last}`
+    `convert #{f} -thumbnail 200x200^ -gravity center -extent 200x200 img/gallery/thumb/#{f.split("/").last}` if !File.exists?("img/gallery/thumb/#{f.split("/").last}")
     tag = f.split("/").last.split("_").first
     img = File.basename(f)
-    html.puts <<-HTML
-      <div class="boxportfolio3 item #{tag}">
-        <div class="boxcontainer">
-          <img src="/img/gallery/thumb/#{img}" alt="">
-          <div class="roll">
-            <div class="wrapcaption">
-              <a data-gal="prettyPhoto[gallery1]" href="/img/gallery/large/#{img}"><i class="icon-zoom-in captionicons"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    HTML
+    width = widths[rand(widths.size)]
+    width = 1 if i == 0
+    html.puts "<div class=\"item #{tag} photo#{width}\"><a data-gal=\"prettyPhoto[gallery]\" href=\"/img/gallery/large/#{img}\"><img src=\"/img/gallery/thumb/#{img}\" alt=\"\"></a></div>"
   end
 end
 
